5196c1588606ef414d92e5469f176e9cf40f29ae
Marcus Willock <marcuswillock@qq.com>
2017-11-01 23:11:12 +0800

    Database schema tweaks
    added cascade to drop tables
    added deleted boolean column
    added timestamp so that I know when the last time something was modified

--- db/tables.sql
+++ db/tables.sql
@@ -1,16 +1,20 @@
-DROP table IF EXISTS class;
+DROP table IF EXISTS class CASCADE;
+DROP table IF EXISTS sex CASCADE;
+DROP table IF EXISTS students CASCADE;
+
 CREATE table class (
-	id		SERIAL PRIMARY KEY,
-	name	VARCHAR(255) NOT NULL UNIQUE
+	id			SERIAL PRIMARY KEY,
+	name		VARCHAR(255) NOT NULL UNIQUE,
+	deleted		BOOLEAN DEFAULT false,
+	timestamp 	TIMESTAMP DEFAULT current_timestamp
 );
 
-DROP table IF EXISTS sex;
 CREATE table sex (
-	id		SERIAL PRIMARY KEY,
-	sex		VARCHAR(255) NOT NULL UNIQUE
+	id			SERIAL PRIMARY KEY,
+	sex			VARCHAR(255) NOT NULL UNIQUE,
+	timestamp 	TIMESTAMP DEFAULT current_timestamp
 );
 
-DROP table IF EXISTS students;
 CREATE table students (
 	id						SERIAL PRIMARY KEY,
 	chinese_name			VARCHAR(255) NOT NULL,
@@ -19,6 +23,8 @@ CREATE table students (
 	student_id				VARCHAR(255) NOT NULL,
 	class_id				INT NOT NULL,
 	sex_id					INT NOT NULL,
+	deleted					BOOLEAN DEFAULT false,
+	timestamp 				TIMESTAMP DEFAULT current_timestamp,
 	FOREIGN KEY(class_id) 	REFERENCES class(id),
 	FOREIGN KEY(sex_id) 	REFERENCES sex(id)	
 );
