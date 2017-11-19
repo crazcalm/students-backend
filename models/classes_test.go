722cc72f3d86c223a6cfe1ce0ab008244af5b2e6
Marcus Willock <marcuswillock@qq.com>
2017-11-19 21:40:37 +0800

    I am still a little confused as to how this mocking api works, but I am making progress with writing these tests

--- models/classes_test.go
+++ models/classes_test.go
@@ -15,7 +15,7 @@ func TestGetClasses(t *testing.T) {
 	//Start the mock
 	db, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatalf("Error %s occured when opening the stub database connection")
+		t.Fatalf("Error %s occured when opening the stub database connection", err.Error())
 	}
 	defer db.Close() // nolint: errcheck
 
@@ -31,7 +31,29 @@ func TestGetClasses(t *testing.T) {
 	if classes[0].ID != testID {
 		t.Fatalf("TestGetClasses: %d is not equal to %d", testID, classes[0].ID)
 	}
-	if strings.EqualFold(testName, classes[0].Name) != true {
+	if !strings.EqualFold(testName, classes[0].Name) {
 		t.Fatalf("TestGetClasses: %s is not equal to %s", testName, classes[0].Name)
+	}
+}
+
+func TestUpdateClassName(t *testing.T) {
+	columns1 := []string{"name"}
+	testName := "Testing"
+	testID := "1"
+
+	//Start the mock
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error %s occurred when opening the stub database connection", err.Error())
+	}
+	defer db.Close() // nolint: errcheck
+
+	mock.ExpectQuery("SELECT (.+) FROM class WHERE (.+)").WillReturnRows(sqlmock.NewRows(columns1).AddRow("hello"))
+	mock.ExpectQuery("UPDATE class SET (.+) WHERE (.+)").WillReturnRows(sqlmock.NewRows(columns1).AddRow(""))
+	mock.ExpectClose()
+
+	err = UpdateClassName(db, testID, testName)
+	if err != nil {
+		t.Fatalf("TestUpdateClassName: err -- %s", err.Error())
 	}
 }
